c7c72d965c3958a88e50451ad7542ed3f77139a7
Marco Antonio Manrique Acha <63822742+MarcoMnrq@users.noreply.github.com>
2021-02-24 19:18:15 -0500

    File Upload

---
+++ src/test/java/com/marcomnrq/consultation/ConsultationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcomnrq.consultation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
