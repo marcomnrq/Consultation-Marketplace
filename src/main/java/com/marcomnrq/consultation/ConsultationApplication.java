7476700c00e31e44ccc689e9c282195af261f5e2
Marco Antonio Manrique Acha <63822742+MarcoMnrq@users.noreply.github.com>
2021-02-24 19:38:41 -0500

    Added Maven Dependencies

--- src/main/java/com/marcomnrq/consultation/ConsultationApplication.java
+++ src/main/java/com/marcomnrq/consultation/ConsultationApplication.java
@@ -1,13 +1,22 @@
 package com.marcomnrq.consultation;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class ConsultationApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ConsultationApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
     }
 
 }
